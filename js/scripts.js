f609340038e3cc67f8645aa207c7be92a931d3f0
Kyle Wymer and Nick Ramsey <kyle@>
2019-03-20 19:54:17 -0700

    add javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,29 @@
-function Account(name, initial) {
+// business logic
+function Account (name, balance) {
   this.name = name;
-  this.initialDeposit = initial deposit;
-}
+  this.balance = balance;
+};
+
+Account.prototype.Deposit = function (deposit) {
+  return this.balance + deposit;
+};
+
+Account.prototype.Withdrawal = function () {
+  return this.balance(balance - withdrawal);
+};
+
+// user interface logic
+$(document).ready(function() {
+  $("form#new-account").submit(function() {
+    debugger;
+    event.preventDefault();
+    var balance = parseInt($("input#initial-deposit").val());
+    var name = ($("#new-first-name").val());
+    var deposit = parseInt($("input#deposit").val());
+    // var withdrawal = parseInt($("input#withdrawal").val());
+    var userAccount = new Account(name, balance);
+    var balance = userAccount.Deposit(deposit);
+    alert(userAccount.balance);
+
+  })
+})
